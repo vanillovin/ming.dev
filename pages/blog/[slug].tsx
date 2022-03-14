86e313404752478964c118a2bcf2ff8d14c7e83b
vanillovin <vanillovin@gmail.com>
2022-03-14 11:52:24 +0900

    스타일 변경 & date 형식 추가

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -17,7 +17,8 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
       {/* prose className을 주면 tailwindcss 플러그인 typography 사용가능 */}
       {/* 커스텀도 가능함. 미량님 깃허브 참고 */}
       <div className="mt-10 prose">
-        <h1 className="text-sky-700">{post.title}</h1>
+        <h1 className="text-gray-700 text-3xl dark:text-gray-300 mb-2">{post.title}</h1>
+        <p className="text-gray-700 dark:text-gray-300 mt-0 text-sm">{post.date.substring(0, 10)}</p>
         <MDXComponent />
       </div>
     </Container>
