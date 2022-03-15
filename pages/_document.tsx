e6819125f0bc80e784a32f7410fc46b618f2f77f
vanillovin <vanillovin@gmail.com>
2022-03-15 20:41:44 +0900

    global font 설정 & 전체적 스타일 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='use-credentials' />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+        </Head>
         <body className="bg-white dark:bg-black text-gray-800 dark:text-gray-300">
           <Main />
           <NextScript />
