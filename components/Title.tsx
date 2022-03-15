825f43fe45c29940f68c03051849b1a4cadce1fe
vanillovin <vanillovin@gmail.com>
2022-03-15 20:38:29 +0900

    Title 수정하고 tailwind custom animation 추가

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,11 +1,26 @@
-import Head from 'next/head';
+import { useTheme } from 'next-themes';
+import Image from 'next/image'
 
 const Title = ({ title, description }: { title: string, description: string }) => {
+  const { theme } = useTheme();
   return (
-    <Head>
-      <title>{title}</title>
-      <p>{description}</p>
-    </Head>
+    <>
+      <div className={`my-8 flex items-end justify-between`}>
+        <span className={`font-extrabold text-7xl text-transparent bg-clip-text bg-gradient-to-r from-amber-400 
+                        to-yellow-600 dark:from-violet-400 dark:to-purple-600 animate-fade-in`}
+        >{title}</span>
+        <div>
+          <span>{description}</span>
+          <Image
+            alt={`em-${theme}`}
+            src={`/static/em-${theme==='dark'?'light':'dark'}.png`}
+            width='20'
+            height='20'
+          />
+        </div>
+      </div>
+      <hr className='border-t-1 border-solid dark:border-gray-600 my-4'/>
+    </>
   )
 } 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in': {
+          'from': { opacity: '1' },
+          'to': { opacity: '0.5' },
+        },
+      },
+      animation: {
+        // animation: name|duration|timing-function|delay|iteration-count|direction
+        'fade-in': 'fade-in 1s alternate infinite linear',
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography")],
   darkMode: 'class',
