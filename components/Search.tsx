c249b7678a1dbff842ae2c188e7c463dbc363d75
vanillovin <vanillovin@gmail.com>
2022-03-15 20:40:43 +0900

    검색 기능 추가

---
+++ components/Search.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Post from './Post';
+import SearchForm from './SearchForm';
+
+const Search = ({ type, posts }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+  const results = searchValue ? filteredPosts : posts;
+
+  return (
+    <>
+      <SearchForm onChange={(e) => setSearchValue(e.target.value)} /> 
+      <div className={`flex flex-col`}>
+        {results.map((post) => (
+          <Post
+            type={type}
+            date={post.date}
+            title={post.title}
+            des={post.description}
+            slug={post.slug}
+            key={post._id}
+          />
+        ))}
+      </div>
+      {!filteredPosts.length && (
+        <p className="ml-1 text-gray-600 dark:text-gray-400">
+          No posts found.
+        </p>
+      )}
+    </>
+  )
+}
+
+export default Search
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,32 +1,26 @@
-import BlogPost from "../components/BlogPost";
-import Container from "../components/Container";
-import { allPosts } from "contentlayer/generated";
 import { InferGetStaticPropsType } from "next";
+
+import Container from "../components/Container";
+import { allBlogs } from 'contentlayer/generated';
 import metadata from 'data/metadata';
+import Title from 'components/Title';
+import Search from 'components/Search';
 
 const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const customMeta = {
     title: `Blog - ${metadata.meta.title}`,
   };
+
   return (
     <Container customMeta={customMeta}>
-      <div className={`mt-10 flex flex-col`}>
-        {posts.map((post) => (
-          <BlogPost
-            date={post.date}
-            title={post.title}
-            des={post.description}
-            slug={post._raw.flattenedPath}
-            key={post._id}
-          />
-        ))}
-      </div>
+      <Title title='Blog' description='개발 및 기술들 공유하는 곳' />
+      <Search type='blog' posts={posts}/>
     </Container>
   );
 };
 
 export const getStaticProps = async () => {
-  const posts = allPosts.sort(
+  const posts = allBlogs.sort(
     (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
   );
 
