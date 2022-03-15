035162a142a3e7a58e361390456d7ad4d45207aa
vanillovin <vanillovin@gmail.com>
2022-03-15 20:36:23 +0900

    rehype 패키지 설치하고 적용 & Note, Blog type 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,18 +1,59 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import rehypeSlug from 'rehype-slug'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrism from 'rehype-prism-plus'
 
-export const Post = defineDocumentType(() => ({
-  name: "Post",
-  contentType: "mdx",
-  filePathPattern: `**/*.mdx`,
+export const Note = defineDocumentType(() => ({
+  name: 'Note',
+  filePathPattern: `note/*.mdx`,
+  contentType: 'mdx',
   fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    // thumbnail: { type: "string", required }
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+    description: { type: 'string', required: false },
   },
-}));
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: doc => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+    },
+  },
+}))
+
+export const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: `blog/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: doc => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+    },
+  },
+}))
 
 export default makeSource({
-  contentDirPath: "posts",
-  documentTypes: [Post],
-});
+  contentDirPath: 'posts',
+  documentTypes: [Blog, Note],
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      rehypePrism,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ['anchor'],
+          },
+        },
+      ],
+    ],
+  },
+})
